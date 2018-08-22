4e905bf180f2211ab0322c39d208f2a70059b204
masa <muramasa8191@MacBook-Air.local>
2018-08-22 22:28:31 +0900

    [fix]syntax error

--- pascal_util.py
+++ pascal_util.py
@@ -33,6 +33,7 @@ def categorical_accuracy_without_ambigyous(y_true, y_pred):
     nb_classes = K.int_shape(y_pred)[-1]
     y_pred = K.reshape(y_pred, (-1, nb_classes))
     
+    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), nb_classes + 1)
     unpacked = tf.unstack(y_true, axis=-1)
     legal_labels = ~tf.cast(unpacked[-1], tf.bool)
     y_true = tf.stack(unpacked[:-1], axis=-1)
