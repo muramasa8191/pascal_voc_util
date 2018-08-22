ada0340d2ffd832739b736760600d0c43eb2f354
masa <muramasa8191@MacBook-Air.local>
2018-08-22 23:27:11 +0900

    [fix]add log

--- pascal_util.py
+++ pascal_util.py
@@ -382,7 +382,9 @@ class VocImageIterator(Iterator):
         
         if class_mode == 'binary':
             return batch_x
-        
+        print (batch_x.shape)
+        print (batch_y.shape)
+
         return batch_x, batch_y
                
 def image_generator(file_paths, size=None, normalization=True):
