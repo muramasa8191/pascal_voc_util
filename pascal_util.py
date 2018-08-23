f19ce625d4e079aa27c6d77dfd87c60c1222ce15
masa <muramasa8191@MacBook-Air.local>
2018-08-23 14:45:41 +0900

    [fix]back to the one with bug

--- pascal_util.py
+++ pascal_util.py
@@ -19,6 +19,7 @@ def crossentropy_without_ambiguous(y_true, y_pred):
     y_pred = K.reshape(y_pred, (-1, K.int_shape(y_pred)[-1]))
     log_softmax = tf.nn.log_softmax(y_pred)
     
+    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1]+1)
     unpacked = tf.unstack(y_true, axis=-1)
     y_true = tf.stack(unpacked[:-1], axis=-1)
     
